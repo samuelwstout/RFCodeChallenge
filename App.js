039abb4d5fba863585611c5b0d6ee4b78b2bf745
samuelwstout <samuelwstout@gmail.com>
2023-02-20 15:15:07 -0600

    build fetch req and render list of NEOs

--- App.js
+++ App.js
@@ -1,19 +1,41 @@
 import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import { Calendar } from 'react-native-calendars';
+import { API_KEY } from "react-native-dotenv";
 
 const App = () => {
 
   const todayDateString = new Date().toISOString().split('T')[0];
 
   const [day, setDay] = useState(todayDateString);
+  const [neos, setNeos] = useState([]);
+  
+  const fetchNeos = () => {
+    fetch(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${day}&end_date=${day}&api_key=${API_KEY}`)
+    .then(r => r.json())
+    .then(data => setNeos(data['near_earth_objects'][day]))
+  }
 
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <Calendar onDayPress={day => setDay(day.dateString)} />
       <Text>{day}</Text>
+      <Button title="Submit" onPress={fetchNeos} />
+      {neos && 
+        neos.map(item => {
+          return (
+            <View key={item.id}>
+              <Text>Name: {item.name}</Text>
+              <Text>Approximate Diameter in Feet: {item.estimated_diameter.feet.estimated_diameter_min} to {item.estimated_diameter.feet.estimated_diameter_max}</Text>
+              <Text>Relative Velocity in mph: {item.close_approach_data[0].relative_velocity.miles_per_hour}</Text>
+              <Text>Miss distance in miles: {item.close_approach_data[0].miss_distance.miles}</Text>
+              <Text>Potentially Hazardous? {item.is_potentially_hazardous_asteroid.toString()}</Text>
+            </View>
+          )
+        })
+      }
     </View>
   );
 }
--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,17 @@
+var path = require('path');
+
 module.exports = function(api) {
-  api.cache(true);
+  api.cache(false);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module:react-native-dotenv',
+        {
+          moduleName: 'react-native-dotenv',
+          configDir: path.resolve(__dirname, "../../")
+        },
+      ],
+    ],
   };
 };
