0ea736de99cc55fcb0f70798604c7a52fe3763a4
samuelwstout <samuelwstout@gmail.com>
2023-02-20 11:17:00 -0600

    build calendar and select day

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Calendar } from 'react-native-calendars';
 
-export default function App() {
+const App = () => {
+
+  const todayDateString = new Date().toISOString().split('T')[0];
+
+  const [day, setDay] = useState(todayDateString);
+
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
       <StatusBar style="auto" />
+      <Calendar onDayPress={day => setDay(day.dateString)} />
+      <Text>{day}</Text>
     </View>
   );
 }
 
+export default App;
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    position: 'relative',
+    top: 20
   },
 });
