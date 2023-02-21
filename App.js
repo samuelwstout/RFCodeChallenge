78178ede68bd9b32f4bd9d3eba6c9c417441a9fb
samuelwstout <samuelwstout@gmail.com>
2023-02-21 01:40:18 -0600

    Add ActivityIndicator. Customize calendar theme

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, Button, ScrollView, ActivityIndicator } from 'react-native';
 import { Calendar } from 'react-native-calendars';
 import { API_KEY } from "react-native-dotenv";
 import moment from 'moment';
@@ -8,34 +8,59 @@ import moment from 'moment';
 const App = () => {
 
   const todayDateString = moment().format("YYYY-MM-DD");
-    
-  const [day, setDay] = useState(todayDateString);
+
+  const [dayCalendar, setDayCalendar] = useState(todayDateString);
+  const [dayFetch, setDayFetch] = useState('');
   const [neos, setNeos] = useState([]);
+  const [loading, setLoading] = useState(false);
   
   const fetchNeos = () => {
-    fetch(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${day}&end_date=${day}&api_key=${API_KEY}`)
+    setLoading(true);
+    fetch(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${dayCalendar}&end_date=${dayCalendar}&api_key=${API_KEY}`)
     .then(r => r.json())
-    .then(data => setNeos(data['near_earth_objects'][day]))
+    .then(data => { 
+      setNeos(data['near_earth_objects'][dayCalendar]);
+      setDayFetch(dayCalendar);
+      setLoading(false);
+    })
   }
 
   return (
-    <View>
+    <View style={styles.background}>
       <StatusBar style="auto" />
       <ScrollView>
       <View style={styles.container}>
         <Text style={styles.text}>Near-Earth Object Finder</Text>
-        <Calendar style={styles.calendar} onDayPress={day => setDay(day.dateString)} />
-        <Button color='#db2020' title={`Click Here to Find NEOs for ${day}`} onPress={fetchNeos} />
+        <Calendar 
+          style={styles.calendar} 
+          onDayPress={day => setDayCalendar(day.dateString)} 
+          markedDates={{ [dayCalendar]: {selected: true, selectedColor: '#078080'}}}
+          theme={{
+            backgroundColor: '#fffffe',
+            calendarBackground: '#fffffe',
+            arrowColor: '#f45d48',
+            textSectionTitleColor: '#232323',
+            monthTextColor: '#232323',
+            textMonthFontWeight: '400',
+          }}
+          />
+        <Button color='#078080' title={`Click Here to Find NEOs for ${dayCalendar}`} onPress={fetchNeos} />
+        {loading && 
+          <ActivityIndicator style={styles.spinner} size='large' />
+        }
+        {neos.length !== 0 && 
+          <Text style={styles.neoTitle}>NEOs for {dayFetch}</Text>
+        }
         <View style={styles.cardContainer}>
-        {neos && 
+        {
           neos.map(item => {
             return (
               <View style={styles.card} key={item.id}>
-                <Text>{item.name}</Text>
-                <Text>Approximate diameter: {Math.round(item.estimated_diameter.feet.estimated_diameter_min)} ft to {Math.round(item.estimated_diameter.feet.estimated_diameter_max)} ft</Text>
-                <Text>Relative velocity: {Math.round(item.close_approach_data[0].relative_velocity.miles_per_hour)} mph</Text>
-                <Text>Miss distance: {Math.round(item.close_approach_data[0].miss_distance.miles)} miles</Text>
-                <Text>Potentially Hazardous? {item.is_potentially_hazardous_asteroid === true ? 'Yes' : 'No'}</Text>
+                <Text style={styles.name}>{item.name}</Text>
+                <Text style={styles.desc}>Approximate diameter: {Math.round(item.estimated_diameter.feet.estimated_diameter_min)} ft to {Math.round(item.estimated_diameter.feet.estimated_diameter_max)} ft</Text>
+                <Text style={styles.desc}>Relative velocity: {Math.round(item.close_approach_data[0].relative_velocity.miles_per_hour)} mph</Text>
+                <Text style={styles.desc}>Miss distance: {Math.round(item.close_approach_data[0].miss_distance.miles)} miles</Text>
+                <Text style={styles.desc}>Potentially Hazardous? {item.is_potentially_hazardous_asteroid === true ? 'Yes' : 'No'}</Text>
               </View>
             );
           })
@@ -50,29 +75,49 @@ const App = () => {
 export default App;
 
 const styles = StyleSheet.create({
+  background: {
+    backgroundColor: '#fffffe'
+  },  
   container: {
     flex: 1,
     padding: 25,
-    marginTop: 10
+    marginTop: 22,
   },
   text: {
     textAlign: 'center',
     fontSize: 20,
+    color: '#232323'
   },
   card: {
     marginBottom: 15,
-    borderColor: '#bacdd8',
-    backgroundColor: '#bacdd8',
+    borderColor: '#f8f5f2',
+    backgroundColor: '#f8f5f2',
     borderWidth: 1,
-    borderRadius: 8,
-    paddingLeft: 6,
-    paddingTop: 4,
-    paddingBottom: 4
+    padding: 10,
   },
   cardContainer: {
-    marginTop: 15
+    marginTop: 15,
   },
   calendar: {
-    marginBottom: 15
+    marginBottom: 15,
+  },
+  neoTitle: {
+    marginTop: 8,
+    textAlign: 'center',
+    fontSize: 20,
+    color: '#094067'
+  },
+  spinner: {
+    marginTop: 8,
+    marginBottom: 8
+  },
+  name: {
+    color: '#232323',
+    fontSize: 20,
+    fontWeight: '500'
+  },
+  desc: {
+    color: '#232323',
+    fontSize: 16
   }
 });
